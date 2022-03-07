1c0056b1264b152ff74398f40832debaf42f01a4
uzal30 <diegomaiauzal@gmail.com>
2022-03-07 20:23:35 -0300

    conflito resolvido, remoção do try e catch

--- connection.php
+++ connection.php
@@ -1,16 +1,10 @@
 <?php
 
-    function getConnection()
-    {
-        try {
-            $key = 'strval';
-            $con = new PDO("mysql:host={$key(HOST)};dbname={$key(DATABASE)}", USER, PASS) or die("Erro ao tentar conectar no banco de dados");
-            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            return $con;
-        } catch (PDOException $error) { # caso não consiga conectar irá entrar no bloco do catch
-            echo "Erro ao conectar ao banco. Erro: {$error->getMessage()}";
-            exit;
-        }
-    }
+    define("HOST", "");
+    define("USER", "");
+    define("PASS", "");
+    define("DATABASE", "");
+
+    $con = new PDO("mysql:host={$key(HOST)};dbname={$key(DATABASE)}", USER, PASS);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
